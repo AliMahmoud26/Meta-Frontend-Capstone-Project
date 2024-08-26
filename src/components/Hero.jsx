838a5561753115768c595be5547ddc0c1113e5e0
AliMahmoud26 <AliMahmoudHelal26@gmail.com>
2024-08-26 23:19:51 +0300

    updating my navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import HeroImg from '../assets/restauranfood.jpg'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='hero'>
+      <div className="container">
+        <div className="hero-text">
+          <h1>Little Lemon</h1>
+          <h4>Chicago</h4>
+          <p>We are a family owned mediterraneran restaurant, focused <br /> on traditional recipes served with a modern twist.</p>
+          <button className='btn'>Reserve Table</button>
+        </div>
+        <div className="hero-img">
+          <img src={HeroImg} alt="hero-image" />
+        </div>
+      </div>
+    </section>
   )
 }
 
