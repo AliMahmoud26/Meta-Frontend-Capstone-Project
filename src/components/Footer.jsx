f2e4bcd267a23bc5beaa697e5db3c50f0575b427
AliMahmoud26 <AliMahmoudHelal26@gmail.com>
2024-08-28 21:50:36 +0300

    adding my footer to the repo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import FooterLogo from '../assets/Logo.svg'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="footer-content">
+        <div className="footer-desc">
+          <img src={FooterLogo} alt="footer logo" />
+          <p>We are a family owned mediterraneran restaurant, focused on traditional recipes served with a modern twist.</p>
+        </div>
+        <div className="link important-links">
+          <h2 className="header">Important Links</h2>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservation</li>
+            <li>Order Online</li>
+            <li>Login</li>
+          </ul>
+        </div>
+        <div className="link contact">
+          <h2 className="header">Contact</h2>
+          <div className="details">
+            <div className="detail">
+              <p>Address:</p>
+              <p>123 Town Street, Chicago</p>
+            </div>
+            <div className="detail">
+              <p>Phone:</p>
+              <p>+00 123 456 789</p>
+            </div>
+            <div className="detail">
+              <p>Email</p>
+              <p>little@lemon.com</p>
+            </div>
+          </div>
+        </div>
+        <div className="link socials">
+          <h2 className="header">Social Media Links</h2>
+          <div className="social-links">
+            <span>Facebook</span>
+            <span>Instagram</span>
+            <span>Twitter</span>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
