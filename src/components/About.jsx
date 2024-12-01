a5540093828ecac3a2204996da3db43744534c34
AliMahmoud26 <AliMahmoudHelal26@gmail.com>
2024-12-01 15:08:25 +0200

    updating my about page content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,9 +10,7 @@ const About = () => {
         <div className="text">
           <h1>Little Lemon</h1>
           <span>Chicago</span>
-          <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
-          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
-          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit...</p>
+          <p>Welcome to our family-owned Mediterranean restaurant, a place where tradition and innovation come together to create an unforgettable dining experience. Our story began with a simple yet profound dream: to share the rich heritage of Mediterranean cuisine with our community. Inspired by recipes passed down through generations, we’ve taken the flavors of our roots and infused them with a modern twist to appeal to today’s adventurous food lovers. From the very beginning, our focus has been on crafting dishes that reflect the warmth and authenticity of our culture...</p>
         </div>
         <div className="images">
           <img src={AboutImg1} alt="about-img" />
